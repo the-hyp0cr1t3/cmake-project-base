219d7fe5bb5b7b9fe4b62c737af67b33233c1fdc
the-hyp0cr1t3 <hriday.gajulapalli@gmail.com>
2022-10-20 23:12:26 +0530

    Update CMake scripts

--- cmake/install-script.cmake
+++ cmake/install-script.cmake
@@ -1,17 +1,17 @@
 include(GNUInstallDirs)
-include(InstallBasicPackageFiles)
-include(AddInstallRPATHSupport)
-include(uninstall-target)
 
-install_basic_package_files(MyProj
-  VERSION ${MyProj_VERSION}
+include(basic-package-install)
+# Export stuff, create config files and all the good stuff :)
+install_basic_package_files(${PROJECT_NAME}
+  VERSION ${${PROJECT_NAME}_VERSION}
   COMPATIBILITY AnyNewerVersion
-  EXPORT MyProj_Runtime
-  VARS_PREFIX MyProj    # what's this?
-  NAMESPACE MyProj::
+  EXPORT ${PROJECT_NAME}_Runtime
+  VARS_PREFIX ${PROJECT_NAME}    # what's this?
+  NAMESPACE ${PROJECT_NAME}::
   NO_SET_AND_CHECK_MACRO
   NO_CHECK_REQUIRED_COMPONENTS_MACRO)
 
+include(support-RPATH)
 # Enable RPATH support for installed binaries and libraries
 add_install_rpath_support(
   BIN_DIRS "${CMAKE_INSTALL_FULL_BINDIR}"

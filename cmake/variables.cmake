0da11965b49b0901e9257e4dca2a534894aaa192
the-hyp0cr1t3 <hriday.gajulapalli@gmail.com>
2022-11-03 12:37:10 +0530

    Add BUILD_EXAMPLES

--- cmake/variables.cmake
+++ cmake/variables.cmake
@@ -1,5 +1,4 @@
 include(CMakeDependentOption)
-include(CMakePrintHelpers)
 
 if(PROJECT_IS_TOP_LEVEL)
 
@@ -11,32 +10,32 @@ if(PROJECT_IS_TOP_LEVEL)
   # consumers can trivially build and package the project
   option(MyProj_DEVELOPER_MODE "Enable developer mode" OFF)
 
+  option(MyProj_BUILD_DOCS "Build documentation using Doxygen and m.css" ON)
+
+  option(MyProj_BUILD_EXAMPLES "Build example programs" ON)
+
+  option(MyProj_BUILD_TESTING "Create tests using CMake" ${MyProj_DEVELOPER_MODE})
+
   cmake_dependent_option(
-    MyProj_BUILD_DOCS
-    "Build documentation using Doxygen and m.css" ON
-    "MyProj_DEVELOPER_MODE" OFF)
+    MyProj_ENABLE_COVERAGE
+    "Build targets with code coverage instrumentation (requires GCC or Clang)" ON
+    "MyProj_BUILD_TESTING" ${MyProj_DEVELOPER_MODE})
 
   cmake_dependent_option(
     MyProj_USE_CLANG_TIDY
     "Run clang-tidy on sources" ON
     "MyProj_DEVELOPER_MODE" OFF)
 
-  cmake_dependent_option(
-    MyProj_BUILD_TESTING
-    "Create tests using CMake" ON
-    "MyProj_DEVELOPER_MODE" OFF)
+  foreach(var IN ITEMS
+            MyProj_DEVELOPER_MODE
+            MyProj_BUILD_DOCS
+            MyProj_BUILD_EXAMPLES
+            MyProj_BUILD_TESTING
+            MyProj_ENABLE_COVERAGE
+            MyProj_USE_CLANG_TIDY)
 
-  cmake_dependent_option(
-    MyProj_ENABLE_COVERAGE
-    "Build targets with code coverage instrumentation (requires GCC or Clang)" ON
-    "MyProj_DEVELOPER_MODE;MyProj_BUILD_TESTING" OFF)
+    message(STATUS "${var}=${${var}}")
 
-  message(STATUS "Project options:")
-  cmake_print_variables(
-    MyProj_DEVELOPER_MODE
-    MyProj_BUILD_DOCS
-    MyProj_USE_CLANG_TIDY
-    MyProj_BUILD_TESTING
-    MyProj_ENABLE_COVERAGE)
+  endforeach()
 
 endif()
